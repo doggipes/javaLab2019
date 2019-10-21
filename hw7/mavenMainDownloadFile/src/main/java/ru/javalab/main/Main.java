36eeece067a624185ac55a79412ffcbdeed47cec
Dzhalil <idzhalil@gmail.com>
2019-10-21 13:08:45 +0300

    hw7

---
+++ hw7/mavenMainDownloadFile/src/main/java/ru/javalab/main/Main.java
@@ -0,0 +1,17 @@
+package ru.javalab.main;
+
+
+
+import ru.javalab.app.urlThread;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < args.length; i++) {
+            urlThread thread = new urlThread(args[i]);
+            thread.start();
+        }
+        }
+    }
+
